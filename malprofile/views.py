338acccd798c61fcb705fc186f231ee50fb2312a
Arthur Frade de Araujo <arthur4raujo@gmail.com>
2024-07-19 00:01:25 -0300

    feat: get profile from my anime list API

--- malprofile/views.py
+++ malprofile/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import os
 import requests
+import json
 
 
 def index(request):
@@ -9,18 +10,37 @@ def index(request):
 
 
 def anime_submit(request):
-    anime_name = request.POST['anime_name']
-    print(anime_name)
-    recommendation = get_recommendation_from_api(anime_name)
-    return HttpResponse(recommendation)
+    profile_name = request.POST['profile_name']
+    print(profile_name)
+    get_top_three_profile_animes(profile_name)
+    recommendations = []
+    for anime in get_top_three_profile_animes(profile_name):
+        anime_name = anime['node']['title']
+        recommendations.append(get_recommendation_from_api(anime_name, 10))
+    
+    return HttpResponse(json.dumps(recommendations), content_type='application/json')
 
 
-def get_recommendation_from_api(anime_name):
-    url = os.environ.get("API_URL")
-    response = requests.get(f'{url}/recommendation?anime_name={anime_name}')
+def get_top_three_profile_animes(profile_name):
+    token = os.environ.get("MAL_API_TOKEN")
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    response = requests.get(f'https://api.myanimelist.net/v2/users/{profile_name}/animelist?sort=list_score&status=completed', headers=headers)
 
     if response.status_code == 200:
-        # Assuming the response is JSON
+        data = response.json()['data']
+        top_three_animes = data[:3]
+        return top_three_animes
+    else:
+        return []
+
+
+def get_recommendation_from_api(anime_name, recommendation_length = 20):
+    url = os.environ.get("API_URL")
+    response = requests.get(f'{url}/recommendation?anime_name={anime_name}&length={recommendation_length}')
+
+    if response.status_code == 200:
         data = response.json()
         return data
     else:
