91f8612acc49e0acb3157fd48dbf5691a206e266
Arthur Frade de Araujo <arthur4raujo@gmail.com>
2024-07-22 13:08:53 -0300

    fix: recommendation list for each watched anime

--- malprofile/views.py
+++ malprofile/views.py
@@ -12,33 +12,31 @@ def index(request):
 
 def anime_submit(request):
     profile_name = request.POST['profile_name']
-    top_three = get_top_three_profile_animes(profile_name)
-    top_three_anime_names = []
-    for anime in top_three:
-        top_three_anime_names.append(anime['node']['title'])
+    best_scored = get_best_scored_watched_animes_form_mal(profile_name)
+    best_scored_anime_names = []
+    for anime in best_scored:
+        best_scored_anime_names.append(anime['node']['title'])
 
-    recommendations = get_recommendations_based_on_list(top_three_anime_names)
+    recommendations = get_recommendations_based_on_list(best_scored_anime_names)
     if len(recommendations) == 0:
         context = {
             "error_message": "No recommendations found"
         }
         return render(request, "malprofile/error.html", context)
     
-    final_recommendations = refine_recommendation(recommendations, top_three_anime_names)
-    if len(final_recommendations) == 0:
-        context = {
-            "error_message": "No recommendations after refining found"
-        }
-        return render(request, "malprofile/error.html", context)
-    
+    recommendations_refined = []
+    for recommendation in recommendations:
+        recommendations_refined.append(refine_recommendation(recommendation))
+        
     context = {
-        "top_three": top_three_anime_names,
-        "recommendations": final_recommendations
+        "recommendations": recommendations_refined
     }
+    
     return render(request, "malprofile/recommendation.html", context)
+    # return HttpResponse(json.dumps(recommendations_refined), content_type="application/json")
 
 
-def get_top_three_profile_animes(profile_name):
+def get_best_scored_watched_animes_form_mal(profile_name):
     token = os.environ.get("MAL_API_TOKEN")
     headers = {
         "Authorization": f"Bearer {token}"
@@ -46,36 +44,42 @@ def get_top_three_profile_animes(profile_name):
     response = requests.get(f'https://api.myanimelist.net/v2/users/{profile_name}/animelist?sort=list_score&status=completed', headers=headers)
 
     if response.status_code == 200:
-        data = response.json()['data']
-        top_three_animes = data[:3]
-        return top_three_animes
+        return response.json()['data']
     else:
         log_error(response)     
 
 
+# gets at least 3 recommendation lists based on the list of animes
 def get_recommendations_based_on_list(anime_list):
+    recommendation_length = 5
+    recommendations_based_on_list = 0
     recommendations = []
     for anime_name in anime_list:
-        recommendations_from_api = get_recommendation_from_api(anime_name, 5)
-        if recommendations_from_api is None:
+        if recommendations_based_on_list == 3:
+            break
+        recommendation_based_on_anime = get_recommendation_from_api(anime_name, recommendation_length)
+        if recommendation_based_on_anime is None:
             continue
-        for item in recommendations_from_api:
-            recommendations.append(item)
+        recommendations.append({
+            'watched_anime': f'{anime_name}',
+            'recommendation': recommendation_based_on_anime
+        })
+        recommendations_based_on_list += 1
     return recommendations
 
 
-def refine_recommendation(recommendations, watched_animes):
-    final_recommendations = []
-    for recommend in recommendations:
-        print(recommend)
-        for name in watched_animes:
-            if re.search(name, recommend):
-                should_include = False
-                break
-            should_include = True
-        if should_include:
-            final_recommendations.append(recommend)
-    return list(dict.fromkeys(final_recommendations))
+def refine_recommendation(recommendation):
+    refined_recommendation = []
+    watched_anime = recommendation['watched_anime']
+    recommendations_based_on_watched = recommendation['recommendation'].keys()
+    for item in recommendations_based_on_watched:
+        if re.search(watched_anime, item):
+            continue
+        refined_recommendation.append({'anime_name': item, 'score': recommendation['recommendation'][item]})
+    return {
+        'watched_anime': recommendation['watched_anime'],
+        'recommendation': refined_recommendation
+    }
 
 
 def get_recommendation_from_api(anime_name, recommendation_length = 20):
